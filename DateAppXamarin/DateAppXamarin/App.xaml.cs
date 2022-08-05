63c2c85e09f7b3c578d4ec8b43f108391dcf103a
David Morrow <quickbasic44@outlook.com>
2022-08-05 17:48:14 -0400

    Add project files.

---
+++ DateAppXamarin/DateAppXamarin/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DateAppXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            AppActions.OnAppAction += AppActions_OnAppAction;
+            MainPage = new MainPage();
+        }
+
+        private void AppActions_OnAppAction(object sender, AppActionEventArgs e)
+        {
+            if (Application.Current != this && Application.Current is App app)
+            {
+                AppActions.OnAppAction -= app.AppActions_OnAppAction;
+                return;
+            }
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                if (e.AppAction.Id == "MainPage")
+                    await Shell.Current.GoToAsync("//" + typeof(MainPage));
+               
+            });
+        }
+
+
+        protected async override void OnStart()
+        {
+            try
+            {
+                await AppActions.SetAsync(
+                    new AppAction("MainPage", "Calculate Dates", icon: "icon.png")); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DateAppXamarin/DateAppXamarin/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DateAppXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        private DateTime _datetime;
+        private TimeSpan _timespan;
+        public DateTime DateTimeResult
+        {
+            get { return _datetime; }
+            set
+            {
+                _datetime = value;
+                OnPropertyChanged();
+            }
+        }
+        public TimeSpan TimeSpanResult
+        {
+            get { return _timespan; }
+            set
+            {
+                _timespan = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            DateTimeResult = DateTime.Now;
+            TimeSpanResult = TimeSpan.FromSeconds(0);
+            BindingContext = this;
+        }
+
+        private TimeSpan DateDifferenceDays(DateTime date)
+        {
+            // find date difference between two dates
+            TimeSpanResult = date - DateTime.Now;
+            return TimeSpanResult;
+        }
+        private DateTime DateAdd(DateTime date, double days = 0, int months = 0, int years = 0, int hours = 0, int minutes = 0)
+        {
+            if (years != 0)
+            {
+               date = date.AddYears(years);
+            }
+            if (months != 0)
+            {
+               date = date.AddMonths(months);
+            }
+            if (days != 0)
+            {
+               date = date.AddDays(days);
+            }
+            if (hours != 0)
+            {
+                date = date.AddHours(hours);
+            }
+            if (minutes != 0)
+            {
+                date = date.AddMinutes(minutes);
+            }
+            DateTimeResult = date;
+            return DateTimeResult;
+
+        }
+
+        private void CalculateDate_Clicked(object sender, EventArgs e)
+        {
+
+           DateTimeResult = DateAdd(DateTime.Now, Double.Parse(EnterDays.Text), Int32.Parse(EnterMonths.Text), Int32.Parse(EnterYears.Text), Int32.Parse(EnterHours.Text), Int32.Parse(EnterMinutes.Text));
+            
+        }
+
+        private void CalculateDifference_Clicked(object sender, EventArgs e)
+        {
+            TimeSpanResult = DateDifferenceDays(DatePicker1.Date);
+
+            if (DatePicker2.Date <= DateTime.Now.AddDays(-1) || DatePicker2.Date >= DateTime.Now.AddDays(1))
+            {
+                TimeSpanResult = DatePicker2.Date - DatePicker1.Date;
+            }
+            
+        }
+    }
+}
